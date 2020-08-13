41cb1bbf37d53d8d1cbfc70c4c0512ac6aad9ae1
Peter Opiyo <peteropiyo@gmail.com>
2020-08-14 00:05:22 +0300

    created the prompt functioin

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-prompt("Which Year were you born?");
+
+//creating the function for asking the user to enter Data
+
+function prompt_user(message){
+  return prompt(message);
+}
+
+
+alert(prompt_user("What is your name?"));
