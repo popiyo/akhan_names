ca080e1908948fac55cbf751ff767eabd46c43c7
Peter Opiyo <peteropiyo@gmail.com>
2020-08-14 03:46:15 +0300

    logic added for cancellation and blank values

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,7 @@ var fullDateEntered;
 var candidateEnglishName;
 var gender; //0 male, 1 female so as to map correctly to the matrix below.
 var stringGender;
+var operationCancelled = true;
 const nameWeekdayMapping = [
   ["Kwasi","Akosua","Sunday"],
   ["Kwadwo","Adwoa", "Monday"],
@@ -41,73 +42,97 @@ const nameWeekdayMapping = [
 
 //prompt prompt_user
 candidateEnglishName = prompt_user("Please enter your Surname: [max 20 characters]\n\n");
+if(candidateEnglishName){
 
-if (!candidateEnglishName) candidateEnglishName = "Candidate";
-if (candidateEnglishName.length >20) candidateEnglishName = candidateEnglishName.substr(0,20);
+      if (!candidateEnglishName) candidateEnglishName = "Candidate";
+      if (candidateEnglishName.length >20) candidateEnglishName = candidateEnglishName.substr(0,20);
 
-dayOfBirth = prompt_user(candidateEnglishName + ",\n\nWhich DATE of the month were your Born [ 1 - 31 ]?\n\n");
-monthofBirth = prompt_user(candidateEnglishName + ",\n\nWhich MONTH were you born [ 1 - 12 ]?\n\n");
-yearOfBirth = prompt_user(candidateEnglishName + ",\n\nWhich YEAR were you born? [ 1000 - 9999 ]\n\n");
-gender = confirm("Please Press OK if you are FEMALE....");
+      dayOfBirth = prompt_user(candidateEnglishName + ",\n\nWhich DATE of the month were your Born [ 1 - 31 ]?\n\n");
+      if(dayOfBirth){
+        monthofBirth = prompt_user(candidateEnglishName + ",\n\nWhich MONTH were you born [ 1 - 12 ]?\n\n");
+        }
 
+      if (monthofBirth){
+        yearOfBirth = prompt_user(candidateEnglishName + ",\n\nWhich YEAR were you born? [ 1000 - 9999 ]\n\n");
 
-//set the gender parameters
-if(gender) {
-  gender = 1;
-  stringGender = "FEMALE";
-} else {
-  gender = 0;
-  stringGender = "MALE";
-}
+      }
 
-fullDateEntered = dayOfBirth + "/" + monthofBirth + "/" + yearOfBirth;
-
-dayOfBirth = parseInt(dayOfBirth);
-monthofBirth = parseInt(monthofBirth);
-yearOfBirth = parseInt(yearOfBirth);
-
-//check that the date values are correct
-
-var validdayOfBirth = (dayOfBirth > 0) && (dayOfBirth < 32);
-var validmonthofBirth = (monthofBirth > 0) && (monthofBirth < 13);
-var validyearOfBirth =(yearOfBirth > 999) && (yearOfBirth < 10000);
-
-if ( validdayOfBirth &&   validmonthofBirth &&  validyearOfBirth) {
-
-      //variables for calculating the days of the week
-      var myCC; //- is the century digits. For example 1989 has CC = 19
-      var myYY; //- is the Year digits (1989 has YY = 89)
-      var scrapvar; //working variable
-      var dayOfWeek; //the week day we are looking for
-      var bioData;
-
-
-      //extract CC
-      scrapvar = yearOfBirth.toString();
-      //capture the first two charactwrs
-      myCC = parseInt (scrapvar[0] + scrapvar[1]);
-      //capture the last two charactwrs
-      myYY = parseInt (scrapvar[2] + scrapvar[3]);
-
-      dayOfWeek = computeDayOfWeek (myCC,myYY,monthofBirth,dayOfBirth);
-      dayOfWeek = parseInt(dayOfWeek.toFixed());
-
-
-
-      bioData = "Based on the data you have provided, we have come to the following Akon cultural deduction for you: " +
-                "\n\nSurname: " + candidateEnglishName + " - " + stringGender +
-                "\nDate of Birth: " + fullDateEntered +
-                "\nDay of Week Born: " + nameWeekdayMapping[dayOfWeek][2] +
-                "\n\nAkon Baptism Name: " + nameWeekdayMapping[dayOfWeek][gender] +
-                "\n\nTHANK YOU FOR SHOWING INTEREST IN OUR AKON CULTURE!" ;
-
-      alert (bioData);
+      if(yearOfBirth)  {
+        operationCancelled = false; //successfully caputured all data
+        gender = confirm("Please Press OK if you are FEMALE....");
+            //set the gender parameters
+            if(gender) {
+              gender = 1;
+              stringGender = "FEMALE";
+            } else {
+              gender = 0;
+              stringGender = "MALE";
+            }
+      }
 
 }
-else {
-  alert(candidateEnglishName + ", \n\nYou have entered and Invalid date: " + fullDateEntered + "\n\nExpected Output: DD/MM/YYYY \nExample: 23/4/2019\n\n");
-}
 
+
+
+
+
+
+//operations only continue if there was no cancellation
+
+if (!operationCancelled)
+{
+      fullDateEntered = dayOfBirth + "/" + monthofBirth + "/" + yearOfBirth;
+
+      dayOfBirth = parseInt(dayOfBirth);
+      monthofBirth = parseInt(monthofBirth);
+      yearOfBirth = parseInt(yearOfBirth);
+
+      //check that the date values are correct
+
+      var validdayOfBirth = (dayOfBirth > 0) && (dayOfBirth < 32);
+      var validmonthofBirth = (monthofBirth > 0) && (monthofBirth < 13);
+      var validyearOfBirth =(yearOfBirth > 999) && (yearOfBirth < 10000);
+
+      if ( validdayOfBirth &&   validmonthofBirth &&  validyearOfBirth) {
+
+            //variables for calculating the days of the week
+            var myCC; //- is the century digits. For example 1989 has CC = 19
+            var myYY; //- is the Year digits (1989 has YY = 89)
+            var scrapvar; //working variable
+            var dayOfWeek; //the week day we are looking for
+            var bioData;
+
+
+            //extract CC
+            scrapvar = yearOfBirth.toString();
+            //capture the first two charactwrs
+            myCC = parseInt (scrapvar[0] + scrapvar[1]);
+            //capture the last two charactwrs
+            myYY = parseInt (scrapvar[2] + scrapvar[3]);
+
+            dayOfWeek = computeDayOfWeek (myCC,myYY,monthofBirth,dayOfBirth);
+            dayOfWeek = parseInt(dayOfWeek.toFixed());
+
+
+
+            bioData = "Based on the data you have provided, we have come to the following Akon cultural deduction for you: " +
+                      "\n\nSurname: " + candidateEnglishName + " - " + stringGender +
+                      "\nDate of Birth: " + fullDateEntered +
+                      "\nDay of Week Born: " + nameWeekdayMapping[dayOfWeek][2] +
+                      "\n\nAkon Baptism Name: " + nameWeekdayMapping[dayOfWeek][gender] +
+                      "\n\nTHANK YOU FOR SHOWING INTEREST IN OUR AKON CULTURE!" ;
+
+            alert (bioData);
+
+      }
+      else {
+        alert(candidateEnglishName + ", \n\nYou have entered and Invalid date: " + fullDateEntered + "\n\nExpected Output: DD/MM/YYYY \nExample: 23/4/2019\n\n");
+      }
+
+}
+else{
+  alert ("\nOperation CANCELLED By User \nOr Blank Value Entered!\n\n");
+}
 
 
 
